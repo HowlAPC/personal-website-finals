6f2558060f318036876ed088af41ad1a7f7c1773
Mj Aragon <mdaragon@student.apc.edu.ph>
2026-02-21 19:14:30 +0000

    please please vercelllll

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,18 +6,12 @@ import express from 'express';
 const server = express();
 
 export const bootstrap = async (expressInstance) => {
-  const app = await NestFactory.create(
-    AppModule, 
-    new ExpressAdapter(expressInstance)
-  );
-  app.enableCors(); // Allows your Vue frontend to talk to this API
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
+  app.enableCors(); // Prevents CORS errors on the frontend
   await app.init();
 };
 
-// Vercel needs to wait for the bootstrap to finish before handling requests
-const handleRequest = async (req: any, res: any) => {
+export default async (req: any, res: any) => {
   await bootstrap(server);
   return server(req, res);
 };
-
-export default handleRequest;
