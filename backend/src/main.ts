9a027083e3666898983eadde3fc3cbba19a32d19
Mj Aragon <mdaragon@student.apc.edu.ph>
2026-02-21 18:48:07 +0000

    babuy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Allows your frontend to talk to the backend
-  if (process.env.NODE_ENV !== 'production') {
-    await app.listen(3000);
-  }
-}
-bootstrap();
+const server = express();
+
+export const bootstrap = async (expressInstance) => {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
+  app.enableCors();
+  await app.init();
+};
+
+bootstrap(server);
+
+// This export is what Vercel looks for
+export default server;
