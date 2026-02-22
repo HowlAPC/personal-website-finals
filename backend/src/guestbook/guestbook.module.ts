d4761fe5c6d7734b57ac7a7ada52838563237443
Mj Aragon <mdaragon@student.apc.edu.ph>
2026-02-22 02:59:07 +0000

    gamble

--- backend/src/guestbook/guestbook.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GuestbookService } from './guestbook.service';
-import { GuestbookController } from './guestbook.controller';
-
-@Module({
-  controllers: [GuestbookController],
-  providers: [GuestbookService],
-})
-export class GuestbookModule {}
